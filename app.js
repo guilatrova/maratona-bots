dbc19dfc634da4cc3e13c93afa3932fc12cf23ac
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-05 08:12:13 -0300

    Added storage settings for Azure

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 var builder = require('botbuilder');
 var restify = require('restify');
+var botbuilder_azure = require("botbuilder-azure");
 var api = require('./api');
 
 const LuisURL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6ffed2ae-7d25-456e-86de-68d97a6bcd1d?subscription-key=376fc739a9034815b3d30eaef6add40f&verbose=true&timezoneOffset=-180&q=";
+const isDebug = false;
 
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
 const connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
 server.post('/api/messages', connector.listen());
 
 const bot = new builder.UniversalBot(connector);
-bot.set('storage', new builder.MemoryBotStorage());
+if (isDebug) {
+    bot.set('storage', new builder.MemoryBotStorage());
+}
+else {
+    const tableName = 'botdata';
+    const azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+    const tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+    bot.set('storage', tableStorage);
+}
 
 const recognizer = new builder.LuisRecognizer(LuisURL);
 const intents = new builder.IntentDialog({ recognizers: [recognizer] })
