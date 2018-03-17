dfcfec43ffb426fb6200f3585116aa3f341c492e
Guilherme Latrova <guilhermelatrova@hotmail.com>
2018-03-17 11:41:35 -0300

    Fixed api to use azure

--- api.js
+++ api.js
@@ -1,17 +1,9 @@
 var request = require('request');
 
-const url = "https://fixdin-api-dev.herokuapp.com/api/v1/expenses/";
-const key = "eb3ab36213e0b5cc82ccf7f626c05774d36b5acd"; //maratona@bots.com / 123456
-const headers = {
-    Authorization: 'Token ' + key,
-    'Content-Type': "application/json"
-};
-const categoryId = 77;
-const accountId = 6;
+const url = "http://maratona-bots-api.azurewebsites.net/api/";
 
-const retrievePendingExpenses = (callback) => {
-    let filterUrl = url + '?payed=0';
-    request.get(filterUrl, { headers: headers }, callback);
+const retrievePendingExpenses = (callback) => {    
+    request.get(url + "expenses", callback);
 };
 
 const createExpense = (entities) => (callback) => {
@@ -23,7 +15,7 @@ const createExpense = (entities) => (callback) => {
         category: categoryId
     };
     const body = JSON.stringify(content);
-    request.post(url, { headers: headers, body: body }, callback);
+    request.post(url + "create", { body: body }, callback);
 };
 
 module.exports = {
